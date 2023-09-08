1dcaeb826f35a0ff0e130ddaca24e293322f0175
Joel Lopez <119460504+djoel95@users.noreply.github.com>
2023-09-08 11:21:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 require('dotenv').config();
 const server = require('./server');
-const { conn } = require('./src/DB_connection');
 
 const { PORT } = process.env;
 
 server.listen(PORT, async () => {
-   await conn .sync({ force: true });
    console.log(`Server raised in port:  + ${PORT}`);
 });
 
